7c6178f6cbdda74570df9719f18dcd40d40c8b13
shimochan1213 <shimokawashadefuel@gmail.com>
2020-08-25 19:37:01 +0900

    オートレイアウト修正

--- GifPractice2/Model/SoundFile.swift
+++ GifPractice2/Model/SoundFile.swift
@@ -11,37 +11,62 @@ import AVFoundation
 
 //音をならすための機能を実装したクラス
 
-class SoundFile{
-    
-    var player:AVAudioPlayer?
-    
-    //ファイル名と拡張子から音を流す処理
-    func playSound(fileName:String, extensionName:String){
+//class SoundFile{
+//
+//    var player:AVAudioPlayer?
+//
+//    //ファイル名と拡張子から音を流す処理
+//    func playSound(fileName:String, extensionName:String){
+//
+//        //再生する
+//        let soundURL = Bundle.main.url(forResource: fileName, withExtension: extensionName)
+//
+//        do{
+//
+//
+//
+//            //効果音を鳴らす
+//            player = try AVAudioPlayer(contentsOf: soundURL!)
+//            player?.play()
+//        }catch {
+//
+//            print("エラーや。")
+//
+//
+//        }
+//
+//    }
+//
+//
+//    }
 
-        //再生する
-        let soundURL = Bundle.main.url(forResource: fileName, withExtension: extensionName)
+    class SoundFile {
 
-        do{
-
-
-
-            //効果音を鳴らす
-            player = try AVAudioPlayer(contentsOf: soundURL!)
-            player?.play()
-        }catch {
-
-            print("エラーや。")
-
-
-        }
-
-    }
-    
-    
-
+    var player: AVAudioPlayer!
 
         
-    }
+        func playSound(fileName: String, extensionName:String) {
+              
+                guard let soundURL = Bundle.main.url(forResource: fileName, withExtension: extensionName)else{
+                    print("音源ファイルが見つかりません")
+                    return
+                }
+                
+                
+                
+                do {
+                    // AVAudioPlayerのインスタンス化
+                    player = try AVAudioPlayer(contentsOf: soundURL)
+                    // 音声の再生
+                    player?.play()
+                } catch {
+                    print("エラー")
+                }
+            }
+        }
+
+
+
     
     
     
